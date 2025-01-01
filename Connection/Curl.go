46bc2c9886ea9c2ae6f8bd3d71f23591c0544015
AmirHossein <amirh.kargar85@gmail.com>
2025-01-02 03:14:13 +0330

    feat: add Curl module

---
+++ Connection/Curl.go
@@ -0,0 +1,80 @@
+package connection
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type Curl struct {
+	Token     string
+	ApiServer string
+}
+
+func NewCurl(token string, apiServer string) *Curl {
+	if apiServer == "" {
+		apiServer = "https://api.telegram.org/"
+	}
+	return &Curl{
+		Token:     token,
+		ApiServer: apiServer,
+	}
+}
+
+func (c *Curl) setURL(method string) string {
+	if len(c.ApiServer) > 0 && c.ApiServer[len(c.ApiServer)-1] == '/' {
+		return fmt.Sprintf("%sbot%s/%s", c.ApiServer, c.Token, method)
+	}
+	return fmt.Sprintf("%s/bot%s/%s", c.ApiServer, c.Token, method)
+}
+
+func (c *Curl) Endpoint(method string, content map[string]interface{}, post bool) (map[string]interface{}, error) {
+	url := c.setURL(method)
+
+	var jsonData []byte
+	var err error
+	if post {
+		jsonData, err = json.Marshal(content)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	var req *http.Request
+	if post {
+		req, err = http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	} else {
+		req, err = http.NewRequest("GET", url, nil)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
